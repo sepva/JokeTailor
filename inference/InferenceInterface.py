4a02de2d3f3a020cfaa97f3c949fc447825fd369
sepva <seppe.vanswegenoven@skynet.be>
2025-03-24 00:15:38 +0100

    added a lot of tests and BoN support

--- inference/InferenceInterface.py
+++ inference/InferenceInterface.py
@@ -2,6 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 from datasets import load_dataset, Dataset
 from RecRAG import RecRag
+from BoN import BoN
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -79,7 +80,12 @@ class InferenceInterface:
             rag = RecRag(**rag_config)
             rag_function = getattr(rag, rag_query_config.pop("method"))
 
-        # TODO: BoN implementation
+        generation_per_userId = 1
+        bon_used = False
+        if not bon_config is None:
+            bon_used = True
+            generation_per_userId = bon_config["extra_gen"]
+            bon_interface = BoN(**bon_config)
 
         def pipeline(userIds):
             suggestions = {}
@@ -105,6 +111,7 @@ class InferenceInterface:
                     add_generation_prompt=True,
                 )
                 for userId in userIds
+                for _ in range(generation_per_userId)
             ]
 
             model_inputs = self.tokenizer(
@@ -121,6 +128,9 @@ class InferenceInterface:
             responses = self.tokenizer.batch_decode(
                 generated_ids, skip_special_tokens=True
             )
+
+            if bon_used:
+                responses = bon_interface.filter_best_responses(responses, userIds)
 
             return responses
 
